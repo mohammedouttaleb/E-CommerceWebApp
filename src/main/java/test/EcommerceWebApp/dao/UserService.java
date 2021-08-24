535807075a19c3c8b5dc690ba1c74a73460b7245
Mohammed Outtaleb <64713679+mohammedouttaleb@users.noreply.github.com>
2021-08-24 13:43:54 +0100

    Update UserService.java

--- src/main/java/test/EcommerceWebApp/dao/UserService.java
+++ src/main/java/test/EcommerceWebApp/dao/UserService.java
@@ -83,12 +83,11 @@ public class UserService  {
 
 
 
-    /*methode pour valider l'inscription d'un nouveau user*/
+  
 	
 	public boolean  UserCheck(User user) {
 		
 		if(user.equals(null)) {
-			//System.out.println("madkhell");
 			return false;
 		}
 			
@@ -99,30 +98,17 @@ public class UserService  {
 		  
 			if(!EmailChecked(user.getEmail())) {
 				System.out.println("madkhell1");
-				
-				//System.out.println(errors.toString());
 				statut=false;
 				
 			}
 			 if(!LoginChecked(user.getLogin())) {
-				
-				System.out.println("madkhel2");
-		
-				//System.out.println(errors.toString());
 				statut=false;
 				
 			}
 			 if(!PasswordChecked(user.getPassword())) {
-				
-				System.out.println("madkhel3");
-				
-				//System.out.println(errors.toString());
 				statut=false;
 			}
-			else {
-				 System.out.println("dkhell"); 
-			     //userrepo.save(user);
-			}
+
 			 
 			return statut;}
 		}
@@ -136,21 +122,15 @@ public class UserService  {
 		
 			for (User user : userlist) {
 				   if(user.getLogin().equals(login)&&user.getPassword().equals(Password)) {
-					   //System.out.println("trouvable");
+					   
 					   return user;
 				   }
 				}
 				errors.put("loginerror", "Login or Password Invalide");
-				//System.out.println("errors "+errors);
-				//System.out.println("introuvable");
 				return null;
 		}
 		errors.put("loginerror", "Login or Password Invalide");
-		//System.out.println("errors "+errors);
-		//System.out.println("introuvable");
 		 return null;
-			
-		
 	}
 	
 	
@@ -179,68 +159,51 @@ public class UserService  {
 		List<User> userlist=userrepo.findAll();
 		
 		if(!email.contains("@")||!email.contains(".com")) {
-			 System.out.println("Email Invalide");
-			 //errors.add("Invalide Email");
 			 errors.put("Email", "Invalide Email");
 			 return false;
 			}
 		else {
 			  for (User user : userlist) {
 				  if(user.getEmail().equals(email)) {
-					System.out.println("Invalide Email**");
-					//errors.add("Invalide Email");
 					errors.put("Email", "Invalide Email***");
-					//System.out.println("errors "+errors.toString());
 					return false;
 					}
 			
 			
 			}
-			  //System.out.println("valide Email");
 			  return true;
 			  }
 		
 		
 	}
 	private boolean LoginChecked(String login) {
-		//System.out.println("Login called");
+		
 		List<User> userlist=userrepo.findAll();
 		
 		 if(login.trim().length()>4&&!login.isEmpty()) {
 			 for (User user : userlist) {
 					if(user.getLogin().contentEquals(login)) {
-						System.out.println("Invalide login");
-						//errors.add("Invalide Login");
 						errors.put("login", "Invalide Login");
-						//System.out.println(errors.toString());
 						return false;	}
 				}
-			 System.out.println("valide Login");
 			 return true;
 		 }
 			 
 		 else {
-			 System.out.println("Invalide login");
-			 //errors.add("Invalide Login");
 			 errors.put("login", "Invalide Login");
-			 //System.out.println(errors.toString());
 			 return false;
 		 }
 			 
 		 }
 	private boolean PasswordChecked(String pass) {
 		
-		//System.out.println("Pass called");
+		
 		if(pass.trim().length()<6||pass.isEmpty()) {
-			System.out.println("Invalide pass");
-			//errors.add("Invalide Password");
 			errors.put("Password", "Invalide Password");
-			System.out.println(errors.toString());
 			return false;
 		}
 			
 		else {
-			System.out.println("valide pass");
 			return true;
 		}
 			
